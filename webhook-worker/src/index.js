31d4db8938570f1764413e8eb5aa73d9b2888270
MachinoLabs <amateuraiguy@gmail.com>
2026-09-12 08:56:58 -0400

    Ln 49 revert

--- webhook-worker/src/index.js
+++ webhook-worker/src/index.js
@@ -46,7 +46,7 @@ export default {
                 
                 const session = await stripe.checkout.sessions.create({
                     ui_mode: 'embedded', // Forces the on-page modal
-                    automatic_payment_methods: { enabled: true },
+                    payment_method_types: ['card'],
                     line_items: [{
                         price: body.priceId,
                         quantity: 1,
